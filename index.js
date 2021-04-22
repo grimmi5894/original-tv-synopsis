290fbdc6856b5e3bd921c01d5c0acf0b8597a285
Tricia Brooker <grimmi5894@gmail.com>
2021-04-21 21:02:43 -0400

    added app.all status 404, and added test header in index.pug

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+
 const showdata = require('./showdata')
 
 const app = express()
 
 app.set('view engine', 'pug')
+
 app.use(express.static('public'))
 
+app.get('/', (request, response) => {
+  return response.render('index', { showdata })
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
+
+app.listen(1337, () => {
+  console.log('Listening on 1337...') // eslint-disable-line no-console
+})
